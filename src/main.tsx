4bc0fabfbc0808aa8e8597ce4a8016756e9bbdb0
sakethram5261 <77665329+sakethram5261@users.noreply.github.com>
2026-04-18 16:49:03 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx' // Note: If your main file is Home.tsx, change this to import { Home } from './Home.tsx' and change <App /> to <Home /> below
+// CHANGED: We are now importing Home instead of App
+import { Home } from './Home.tsx' 
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 
-// This pulls the key you saved in Vercel's Environment Variables
+// Pulls the key from Vercel's Environment Variables
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
@@ -14,7 +15,8 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
+      {/* CHANGED: We render Home here instead of App */}
+      <Home /> 
     </ClerkProvider>
   </React.StrictMode>,
 )
